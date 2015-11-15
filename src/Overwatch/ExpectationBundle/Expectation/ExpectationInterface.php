0fbab8e8bf414bad232759423479ebe17b67210f
Amy Boyd <amy@amyboyd.co.uk>
2015-11-15 09:46:14 +0000

    Document ExpectationInterface::run().

--- src/Overwatch/ExpectationBundle/Expectation/ExpectationInterface.php
+++ src/Overwatch/ExpectationBundle/Expectation/ExpectationInterface.php
@@ -8,5 +8,17 @@ namespace Overwatch\ExpectationBundle\Expectation;
  * The code to run a test goes inside the run() function.
  */
 interface ExpectationInterface {
+	/**
+	 * Attempt to execute the expectation, resulting in a textual result if successful, or throwing
+	 * an exception if the expectation failed.
+	 *
+	 * @param  string $actual
+	 * @param  string $expected
+	 * @return string
+	 * @throws ExpectationFailedException
+	 * @throws ExpectationNotFoundException
+	 * @throws ExpectationResultException
+	 * @throws ExpectationUnsatisfactoryException
+	 */
     public function run($actual, $expected = NULL);
 }
